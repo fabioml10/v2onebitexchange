c310d6a484492b9f46d99041d86ad131f0bb7ab7
Fabio Muller <fabioml10@yahoo.com.br>
2020-01-11 00:53:32 -0300

    exchange service part 2

--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -3,18 +3,19 @@ require 'json'
 
 class ExchangeService
   def initialize(source_currency, target_currency, amount)
-    p @source_currency = source_currency
-    p @target_currency = target_currency
-    p @amount = amount.to_f
+    @source_currency = source_currency
+    @target_currency = target_currency
+    @amount = amount.to_f
   end
  
   def perform
     begin
-      exchange_api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
-      exchange_api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
+      # TODO:Não consegue pegar a informação do credencials, implementar ENVs
+      # exchange_api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
+      # exchange_api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
 
-      # p exchange_api_url = 'https://currencydatafeed.com/api/data.php'
-      # p exchange_api_key = 'a1kpdyj5warenkzatf6a'
+      exchange_api_url = 'https://currencydatafeed.com/api/data.php'
+      exchange_api_key = 'a1kpdyj5warenkzatf6a'
 
       url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
       res = RestClient.get url
